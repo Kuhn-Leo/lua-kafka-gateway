522aaef739ce5e9da1a044892738fbd177b41dc4
coderlikeadog <yilei@htang.com.cn>
2018-05-08 11:15:47 +0800

    remove log for test & uncomment kafka producer

--- lib/kafka/gateway/send.lua
+++ lib/kafka/gateway/send.lua
@@ -4,7 +4,6 @@ local config       = require "kafka.gateway.config"
 
 local log          = ngx.log
 local ERR          = ngx.ERR
-local NOTICE       = ngx.NOTICE
 local json_encode  = cjson.encode
 local msg_list     = ngx.ctx.messageList
 local BROKER_LIST  = config.BROKER_LIST
@@ -12,21 +11,18 @@ local KAFKA_CONFIG = config.KAFKA_CONFIG
 
 if msg_list then
   -- this is async producer_type and bp will be reused in the whole nginx worker
-  -- local kafka_producer = producer:new(BROKER_LIST, KAFKA_CONFIG)
+  local kafka_producer = producer:new(BROKER_LIST, KAFKA_CONFIG)
   for _, v in ipairs(msg_list) do
     if v then
       local topic = v["tpc"]
       local key   = v["sig"]
       local data  = json_encode(v)
       if topic then
-        --[[
         local ok, err = kafka_producer:send(topic, key, data)
         if not ok then
           log(ERR, "kafka err: " .. err)
           log(ERR, "err msg: " .. data)
         end
-        --]]
-        log(NOTICE, "msg: " .. data)
       end
     end
   end
